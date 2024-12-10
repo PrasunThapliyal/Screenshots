268efce34339923d3c8cc48f8c20c4f1526e1769
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2024-12-10 14:33:27 +0530

    Add a keyboard shortcut to capture screenshot

--- background.js
+++ background.js
@@ -6,3 +6,22 @@
 //       });
 //     }
 //   });
+
+chrome.commands.onCommand.addListener((shortcut) => {
+  if (shortcut === "captureNow") {
+    captureNow();
+  }
+});
+
+export function captureNow() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+      // TODO : We can send a message to a background worker as well, but don't know atm what's the benifit
+      // chrome.runtime.sendMessage({ action: 'saveImage', data: dataUrl });
+      chrome.downloads.download({
+        url: dataUrl,
+        filename: "screenshotB.png",
+      });
+    });
+  });
+}
--- main/main.js
+++ main/main.js
@@ -1,15 +1,5 @@
-document.getElementById('captureNow').addEventListener('click', () => {
+import { captureNow } from "../background.js";
 
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.captureVisibleTab(null, {format:"png"}, (dataUrl) => {
-            // TODO : We can send a message to a background worker as well, but don't know atm what's the benifit
-            // chrome.runtime.sendMessage({ action: 'saveImage', data: dataUrl });
-            
-            chrome.downloads.download({
-                url: dataUrl,
-                filename: 'screenshotA.png'
-              });
-        });
-    });
-  });
-  
+document.getElementById("captureNow").addEventListener("click", () => {
+  captureNow();
+});
